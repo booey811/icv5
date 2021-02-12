b53863cb3d387404c2fc41688de1687294570340
Gabriel Barr <gabrielbarr811@gmail.com>
2021-02-12 11:56:30 +0000

    Remove unify topline

--- icv5/components/unify.py
+++ icv5/components/unify.py
@@ -7,7 +7,3 @@ from moncli import ColumnType
 from icv5.components.monday.boardItems_misc import StuartDataItem
 from icv5.components.monday import boardItems_main, boardItems_inventory, manage, boardItems_reporting
 from icv5.components.stuart import stuart
-
-test = boardItems_reporting.FinancialItem(1046233015)
-
-test.process_repair_data()
