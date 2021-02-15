44fcc2c5bd4bbc0bafbfb0b45d351943c4683318
Gabriel Barr <gabrielbarr811@gmail.com>
2021-02-15 09:49:50 +0000

    Adding Production .env Switch for Stuart

--- icv5/components/unify.py
+++ icv5/components/unify.py
@@ -1,9 +1,16 @@
 from pprint import pprint as p
 import time
+import os
 
 from moncli.entities import create_column_value
 from moncli import ColumnType
+import settings
 
 from icv5.components.monday.boardItems_misc import StuartDataItem
 from icv5.components.monday import boardItems_main, boardItems_inventory, manage, boardItems_reporting
 from icv5.components.stuart import stuart
+
+
+thing = stuart.StuartClient()
+
+print(thing.production)
