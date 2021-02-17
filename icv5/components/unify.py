831c4439f848d1ff01b49ba7ee986bff29ef2940
Gabriel Barr <gabrielbarr811@gmail.com>
2021-02-17 10:54:20 +0000

    Courier Booking Route Resolved

--- icv5/components/unify.py
+++ icv5/components/unify.py
@@ -12,11 +12,7 @@ from icv5.components.monday.boardItems_misc import StuartDataItem
 from icv5.components.monday import boardItems_main, boardItems_inventory, boardItems_reporting, manage
 from icv5.components.stuart import stuart
 from icv5.components.zendesk import ticket
+from icv5.components.zendesk.assets import custom_fields
 
 from pprint import pprint as p
 
-test = ticket.ZendeskTicket(8345)
-
-for field in test.ticket.custom_fields:
-
-    print(field)
