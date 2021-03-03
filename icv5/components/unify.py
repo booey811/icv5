294126b0ab94f89d5449ce8ef0eb85c423d0c87a
Gabriel Barr <gabrielbarr811@gmail.com>
2021-03-03 09:56:34 +0000

    Courier Dump Changes

--- icv5/components/unify.py
+++ icv5/components/unify.py
@@ -15,18 +15,7 @@ from icv5.components.zendesk import ticket
 from icv5.components.zendesk.assets import custom_fields
 
 from pprint import pprint as p
-
-webhook = {'event': {'userId': 4251271}}
-
-item_id = 1092341311
-
-main_item = boardItems_main.MainBoardItem(item_id=item_id, webhook_payload=webhook)
-
-courier = stuart.StuartClient(main_item=main_item)
-
-try:
-    job_payload = courier.validate_job_details('delivery')
-    courier.book_courier_job(job_payload)
-except (stuart.DistanceTooGreat or stuart.EmailInvalid or stuart.CannotGeocodeAddress or
-        stuart.UnknownValidationError or stuart.CourierDetailsMissing or stuart.PhoneNumberInvalid):
-    pass
+# 
+# webhook = {'event': {'userId': 4251271}}
+# 
+# item_id = 1092341311
