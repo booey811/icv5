e6e7479fbfcc939076105a4feb07df553ffc6d42
Gabriel Barr <gabrielbarr811@gmail.com>
2021-03-09 15:40:05 +0000

    Corretion for untracked items during stock checker

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,14 @@ import pytest
 @pytest.fixture()
 def blank_main_object():
     from icv5.components.monday.boardItems_main import MainBoardItem
-
     return MainBoardItem()
+
+
+@pytest.fixture()
+def return_gabe_user_id():
+    return '4251271'
+
+
+@pytest.fixture()
+def return_sample_webhook_payload():
+    return {'event': {'userId': '4251271'}}
